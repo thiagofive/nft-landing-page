f12510df7ec55182aa3013d5985cfbe1d3a97d9e
thiagofive <65563172+thiagofive@users.noreply.github.com>
2022-09-20 22:59:49 -0300

    Bug Fixes

--- functions/isowner.js
+++ functions/isowner.js
@@ -2,7 +2,7 @@ const fetch = require('node-fetch')
 
 const CONTRACT = process.env.CONTRACT_ADDRESS;
 const AUTH = process.env.NFTPORT_AUTH;
-const chain = "mumbai";
+const chain = "Mumbai";
 const include = "metadata";
 
 exports.handler = async (event, context) => {
